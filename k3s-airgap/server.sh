ba7783dd84481f91891bd59e7510bd2edda21ecf
kuaizi <qhethe525@gmil.com>
2024-05-16 18:01:23 +0800

    feat(k3s-airgap/server.sh): cluster init prompt

--- k3s-airgap/server.sh
+++ k3s-airgap/server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 read -p "External IP: " EXTERNALIP
 read -p "Node IP: " NODEIP
+read -P "Cluster Init (yes/no): " answer
+
+export K3S_TOKEN=$(pwgen 25 1)
+export INSTALL_K3S_SKIP_DOWNLOAD=true
 
 # Copy files
 mkdir -p /var/lib/rancher/k3s/agent/images
@@ -8,16 +12,28 @@ cp k3s-airgap-images-amd64.tar.zst /var/lib/rancher/k3s/agent/images
 cp k3s /usr/local/bin/k3s
 chmod a+x /usr/local/bin/k3s
 
-export K3S_TOKEN=$(pwgen 25 1)
-export INSTALL_K3S_SKIP_DOWNLOAD=true
-
-curl -sfL https://get.k3s.io | sh -s - server \
-    --cluster-init \
-    --flannel-backend=wireguard-native \
-    --flannel-iface=wg0 \
-    --node-ip=$NODEIP \
-    --advertise-address=$NODEIP \
-    --node-external-ip=$EXTERNALIP \
-    --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
+if [ "$answer" = "yes" ]; then
+    read -p "Server (External IP): " SERVER
+    curl -sfL https://get.k3s.io | sh -s - server \
+        --cluster-init \
+        --flannel-backend=wireguard-native \
+        --flannel-iface=wg0 \
+        --node-ip=$NODEIP \
+        --advertise-address=$NODEIP \
+        --node-external-ip=$EXTERNALIP \
+        --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
+elif [ "$answer" = "no" ]; then
+    curl -sfL https://get.k3s.io | sh -s - server \
+        --server=$SERVER\
+        --flannel-backend=wireguard-native \
+        --flannel-iface=wg0 \
+        --node-ip=$NODEIP \
+        --advertise-address=$NODEIP \
+        --node-external-ip=$EXTERNALIP \
+        --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
+else
+    echo "Invalid input. Please enter 'yes' or 'no'."
+    exit 0;
+fi
 
 echo "K3S_TOKEN: $(tee token < $K3S_TOKEN)"
