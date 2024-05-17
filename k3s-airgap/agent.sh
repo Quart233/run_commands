1e2a565930b90b0d4c9edabfec4f794ea5c78d38
kuaizi <qhethe525@gmil.com>
2024-05-17 15:01:09 +0800

    feat: k3s agent.sh

---
+++ k3s-airgap/agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "External IP: " EXTERNALIP
+read -p "Node IP: " NODEIP
+read -p "K3S_URL (https://<register-ip>:6443): " K3S_URL 
+read -p "Token: " K3S_TOKEN
+
+export K3S_URL
+export K3S_TOKEN
+export INSTALL_K3S_SKIP_DOWNLOAD=true
+
+# Copy files
+echo "Copying Files..."
+mkdir -p /var/lib/rancher/k3s/agent/images
+cp k3s-airgap-images-amd64.tar.zst /var/lib/rancher/k3s/agent/images
+cp k3s /usr/local/bin/k3s
+chmod a+x /usr/local/bin/k3s
+
+curl -sfL https://get.k3s.io | sh -s - agent \
+    --flannel-iface=wg0 \
+    --node-ip=$NODEIP \
+    --node-external-ip=$EXTERNALIP
