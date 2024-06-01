304fc4330022696ac70adf5c387ec12844f921f7
kuaizi <qhethe525@gmail.com>
2024-06-01 18:41:10 +0800

    feat(k3s-airgap/server.sh): update flannel mode host-gw

--- k3s-airgap/server.sh
+++ k3s-airgap/server.sh
@@ -16,12 +16,11 @@ if [ "$answer" = "yes" ]; then
     export K3S_TOKEN=$(pwgen 25 1 | tee token)
     curl -sfL https://get.k3s.io | sh -s - server \
         --cluster-init \
-        --flannel-backend=vxlan \
+        --flannel-backend=host-gw \
         --flannel-iface=wg0 \
-        --node-ip=$NODEIP \
         --advertise-address=$NODEIP \
-        --node-external-ip=$EXTERNALIP \
         --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
+
     echo "K3S_TOKEN: $K3S_TOKEN"
 elif [ "$answer" = "no" ]; then
     read -p "K3S_URL (https://<register-ip>:6443): " K3S_URL 
@@ -29,11 +28,9 @@ elif [ "$answer" = "no" ]; then
     export K3S_URL
     export K3S_TOKEN
     curl -sfL https://get.k3s.io | sh -s - server \
-        --flannel-backend=vxlan \
+        --flannel-backend=host-gw \
         --flannel-iface=wg0 \
-        --node-ip=$NODEIP \
         --advertise-address=$NODEIP \
-        --node-external-ip=$EXTERNALIP \
         --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
 else
     echo "Invalid input. Please enter 'yes' or 'no'."
