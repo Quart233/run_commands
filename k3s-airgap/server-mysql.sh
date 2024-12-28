d3e7f1b5d7bf431f4eecb0d84b84489599016bd8
kuaizi <qhethe525@gmail.com>
2024-12-28 11:07:44 +0800

    feat(k3s-airgap): K3S Mysql Helper Script

--- k3s-airgap/server-mysql.sh
+++ k3s-airgap/server-mysql.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-read -p "External IP: " EXTERNALIP
-read -p "Node IP: " NODEIP
-read -p "K3S_DATASTORE_ENDPOINT: " K3S_DATASTORE_ENDPOINT
-read -p "Cluster Init (yes/no): " answer
+read -p "Init (yes/no): " answer
 
 export INSTALL_K3S_SKIP_DOWNLOAD=true
 
@@ -13,20 +10,28 @@ cp k3s-airgap-images-amd64.tar.zst /var/lib/rancher/k3s/agent/images
 cp k3s /usr/local/bin/k3s
 chmod a+x /usr/local/bin/k3s
 
+read -p "Node IP: " NODEIP
+read -p "DB_HOSTNAME: " DB_HOSTNAME
+read -p "DB_USERNAME: " DB_USERNAME
+read -p "DB_PASSWORD: " DB_PASSWORD
+
 if [ "$answer" = "yes" ]; then
-    export K3S_TOKEN=$(pwgen 25 1 | tee token)
+    read -p "External IP: " EXTERNALIP
     curl -sfL https://get.k3s.io | sh -s - server \
+        --token=$(pwgen 25 1 | tee secret) \
+        --datastore-endpoint="mysql://$DB_USERNAME:$DB_PASSWORD@tcp($DB_HOSTNAME:3306)/k3s" \
         --advertise-address=$NODEIP \
-        --egress-selector-mode=disabled \
         --tls-san=$EXTERNALIP # Optional, needed if using a fixed registration address
 
-    echo "K3S_TOKEN: $K3S_TOKEN"
+    # Token
+    cat /var/lib/rancher/k3s/server/token
+
 elif [ "$answer" = "no" ]; then
     read -p "Token: " K3S_TOKEN
-    export K3S_TOKEN
     curl -sfL https://get.k3s.io | sh -s - server \
-        --advertise-address=$NODEIP \
-        --egress-selector-mode=disabled \
+        --token="$K3S_TOKEN" \
+        --datastore-endpoint="mysql://$DB_USERNAME:$DB_PASSWORD@tcp($DB_HOSTNAME:3306)/k3s" \
+        --advertise-address=$NODEIP
 else
     echo "Invalid input. Please enter 'yes' or 'no'."
     exit 0;
